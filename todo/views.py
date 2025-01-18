4fc9b23df69faba46c932d5345ff4ac8255cc36a
unknown <heydari1375.ali@gmail.com>
2025-01-18 15:27:52 +0330

    create list view on todo.views and migrate for database

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Todo
+from django.views.generic import ListView
 # Create your views here.
+
+
+
+
+class TodoListView(ListView):
+    model = Todo
+    context_object_name = 'todos'
