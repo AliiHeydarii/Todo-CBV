c46b69a269539aaf7f2bd3a57a88455e2f9999a8
unknown <heydari1375.ali@gmail.com>
2025-01-18 16:23:27 +0330

    add create view for add new tood

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('' , views.TodoListView.as_view() , name='todo-list'),
-    path('todo-detail/<int:pk>/' ,  views.TodoDetial.as_view() , name='todo-detial')
+    path('todo-detail/<int:pk>/' ,  views.TodoDetial.as_view() , name='todo-detial'),
+    path('todo-create/' , views.TodoCreate.as_view() , name='todo-create')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Todo
-from django.views.generic import ListView , DetailView
+from django.views.generic import ListView , DetailView , CreateView
+from .forms import TodoForm
 # Create your views here.
 
 
@@ -16,3 +17,9 @@ class TodoDetial(DetailView):
     model = Todo
     context_object_name = 'todo'
     template_name = 'todo_detail.html'
+
+
+class TodoCreate(CreateView):
+    form_class = TodoForm
+    template_name = 'todo_create.html'    
+    success_url = '/'
