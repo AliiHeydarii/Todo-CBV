36303f2ff34d6302a6b57b50df6bd095e1fc90bd
unknown <heydari1375.ali@gmail.com>
2025-01-18 15:32:23 +0330

    add __str__ mehtod on Todo Model

--- todo/models.py
+++ todo/models.py
@@ -14,3 +14,7 @@ class Todo(models.Model):
     description = models.TextField()
     status = models.CharField(max_length=15 , choices=TodoStatus.choices)
     create_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
