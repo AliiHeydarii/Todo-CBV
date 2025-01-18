356840651f4cb466843671b278428404f27e7d84
unknown <heydari1375.ali@gmail.com>
2025-01-18 16:09:24 +0330

    add detail view and template for that

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('' , views.TodoListView.as_view() , name='todo-list')
+    path('' , views.TodoListView.as_view() , name='todo-list'),
+    path('todo-detail/<int:pk>/' ,  views.TodoDetial.as_view() , name='todo-detial')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Todo
-from django.views.generic import ListView
+from django.views.generic import ListView , DetailView
 # Create your views here.
 
 
@@ -10,3 +10,9 @@ class TodoListView(ListView):
     model = Todo
     context_object_name = 'todos'
     template_name = 'todo_list.html'
+
+
+class TodoDetial(DetailView):
+    model = Todo
+    context_object_name = 'todo'
+    template_name = 'todo_detail.html'
