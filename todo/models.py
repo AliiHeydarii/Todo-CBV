37f53e0b8cf92afff1fc81c6f9cb96f74927862c
unknown <heydari1375.ali@gmail.com>
2025-01-18 15:14:22 +0330

    create urls.py for todo app and create todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    class TodoStatus(models.TextChoices):
+        DONE = 'Done' , 'Done'
+        PENDING = 'Pending' , 'Pending'
+        NOTSTARTED = 'NotStarted' , 'NotStarted'
+
+
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    status = models.CharField(max_length=15 , choices=TodoStatus.choices)
+    create_at = models.DateTimeField(auto_now_add=True)
